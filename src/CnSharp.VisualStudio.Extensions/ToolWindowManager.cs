5033c1af95b1c88edc68126a1b113e77fd9d4c03
Bob Chin <cnsharp@yeah.net>
2025-04-04 00:17:33 +0800

    Add ToolWindow support.

---
+++ src/CnSharp.VisualStudio.Extensions/ToolWindowManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CnSharp.VisualStudio.Extensions
+{
+    public class ToolWindowManager
+    {
+        private static readonly Dictionary<string, int> WindowBaseIds = new Dictionary<string, int>();
+        private static readonly Dictionary<string, int> WindowIdUsed = new Dictionary<string, int>();
+        private static readonly Dictionary<string, int> WindowIds = new Dictionary<string, int>();
+
+        public static void RegisterWindowBaseId(Type type, int baseId)
+        {
+            WindowBaseIds[type.FullName] = baseId;
+        }
+
+        public static int GetWindowId(Type type, string uri)
+        {
+            var key = type.FullName + "/" + uri;
+            if (WindowIds.ContainsKey(key))
+            {
+                return WindowIds[key];
+            }
+
+            if (WindowIdUsed.ContainsKey(type.FullName))
+            {
+                var id = WindowIdUsed[type.FullName] + 1;
+                WindowIdUsed[type.FullName] = id;
+                WindowIds[key] = id;
+                return id;
+            }
+            var baseId = WindowBaseIds[type.FullName];
+            WindowIdUsed[type.FullName] = baseId;
+            WindowIds[key] = baseId;
+            return baseId;
+        }
+    }
+}
--- src/CnSharp.VisualStudio.Extensions/WindowAccessor.cs
+++ src/CnSharp.VisualStudio.Extensions/WindowAccessor.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Windows.Forms;
 using EnvDTE;
 using EnvDTE80;
 
@@ -11,8 +10,6 @@ namespace CnSharp.VisualStudio.Extensions
     {
         public Windows2 Windows { get; set; }
 
-        [Obsolete]
-        public AddIn AddIn { get; set; }
 
         public  Window FindWindowByGuid(string guid)
         {
@@ -48,44 +45,6 @@ namespace CnSharp.VisualStudio.Extensions
             return windowList.FirstOrDefault(expression);
         }
 
-        [Obsolete]
-        public  object AddWindow(WindowInfo window)
-        {
-            var myWindow = Windows;
-            string assemblypath = window.AssemblyLocation;
-            AddIn addinobj = AddIn;
-
-
-            Object obj = null;
-            Window newWinobj = myWindow.CreateToolWindow2(addinobj, assemblypath,
-                window.ClassName, window.Caption, window.Guid, ref obj);
-
-            newWinobj.Visible = true;
-            if (window.Picture != null)
-                newWinobj.SetTabPicture(window.Picture);
-            //以下两句实现文档式停靠
-            if (window.IsDocument)
-            {
-                newWinobj.IsFloating = false;
-                newWinobj.Linkable = false;
-            }
-            else
-            {
-                if (window.Width > 0)
-                    newWinobj.Width = window.Width;
-                if (window.Height > 0)
-                    newWinobj.Height = window.Height;
-
-                if (window.LinkToWindow != null)
-                    myWindow.CreateLinkedWindowFrame(window.LinkToWindow, newWinobj,
-                        vsLinkedWindowType.vsLinkedWindowTypeTabbed);
-            }
-
-
-            return obj;
-        }
-
-
         /// <summary>
         ///     output message to Output Window
         /// </summary>
@@ -112,26 +71,7 @@ namespace CnSharp.VisualStudio.Extensions
             pane.Activate();
 
             // Add a line of text to the new pane.
-            pane.OutputString(message + "\r\n");
-        }
-
-        [Obsolete]
-        public  T NewWindow<T>(WindowInfo win) where T : UserControl
-        {
-            Window window = FindWindowByCaption(win.Caption);
-            T view = null;
-            if (window != null)
-            {
-                window.Activate();
-                view = window.Object as T;
-            }
-            if (view == null)
-            {
-                win.ClassName = typeof (T).FullName;
-                object obj = AddWindow(win);
-                view = obj as T;
-            }
-            return view;
+            pane.OutputString(message + Environment.NewLine);
         }
     }
 }
