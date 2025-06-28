9ee892bf759ad3f758f78d3e6e7b6afe583e27ca
Bob Chin <cnsharp@yeah.net>
2025-06-28 22:45:56 +0800

    Add controls.

---
+++ src/CnSharp.VisualStudio.Extensions/Controls/WinFormsUserControlHolderControl.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Controls;
+
+namespace CnSharp.VisualStudio.Extensions.Controls
+{
+    /// <summary>
+    /// WPF UserControl to hold a windows forms UserControl in it.
+    /// </summary>
+    public partial class WinFormsUserControlHolderControl : UserControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WinFormsUserControlHolderControl"/> class.
+        /// </summary>
+        public WinFormsUserControlHolderControl(System.Windows.Forms.UserControl winFormsControl)
+        {
+            this.InitializeComponent();
+
+            this.winFormsHost.Child = winFormsControl;
+        }
+    }
+}
